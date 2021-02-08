071130f1a91124a383cb93530f8f2a424c8a8e71
michael robles <dokosatchii@protonmail.com>
2021-02-08 17:37:32 -0500

    loaded episodes and movies

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,34 +1,113 @@
-var data = [
-    ["movie1","ep1","AD"]
-    ["movie1","ep1","lang1"]
-    ["show1","ep1","AD"]
-    ["show1","ep1","lang1"]
-    ["show1","ep1","lang2"]    
-    ["show1","ep2","AD"]
-    ["show1","ep2","lang1"]
-    ["show1","ep2","lang2"]    
-    ["show1","ep3","AD"]
-    ["show1","ep3","lang1"]
-    ["show1","ep3","lang2"]
-    
+// var data = [
+//     ["movie1","ep1","AD"],
+//     ["movie1","ep1","lang1"],
+//     ["show1","ep1","AD"],
+//     ["show1","ep1","lang1"],
+//     ["show1","ep1","lang2"],    
+//     ["show1","ep2","AD"],
+//     ["show1","ep2","lang1"],
+//     ["show1","ep2","lang2"],    
+//     ["show1","ep3","AD"],
+//     ["show1","ep3","lang1"],
+//     ["show1","ep3","lang2"]
+// ];
+
+var movieTitles = [
+    "White Tiger",
+    "Umbrella Academy"
 ];
 
-const filteredArray = data.filter(r => r[0] === "movie1");
+var VideoData = {
+    "White Tiger": [
+        {
+            "title": "episode 1",
+            "languages": [
+                "language 1",
+                "language 2",
+                "AD"
+            ]
+        }, {
+            "title": "episode 2",
+            "languages": [
+                "language 1",
+                "language 2",
+                "AD"
+            ]
+        }
+    ],
+    "Umbrella Academy": [
+        {
+            "title": "EPISODE 1",
+            "languages": [
+                "language 1",
+                "language 2",
+                "AD"
+            ]
+        }, {
+            "title": "EPISODE 2",
+            "languages": [
+                "language 1",
+                "language 2",
+                "AD"
+            ]
+        }
+    ]
+}
 
-const uniqueOption = new Set();
-filteredArray.forEach(r => uniqueOption.add(r[1]));
+function loadEpisodes(event) {
+    var name = $(event.target).attr('name');
+    console.log(name);
+    $("#titleList").empty()
+    $("#selectTitle").text("Select Episode")
+    VideoData[name].map(episode => {
+        console.log(episode['title']);
+        $("#selectTitle").append(
+            $("<div>")
+                .attr("id", episode['title'])
+            .text(episode['title'])
+            .append("<div>"+episode['languages']+"</div")
+        );
+    })
+}
 
-const uniqueList = [...uniqueOptions];
+function loadMovies() {
+    $("#titleList").empty()
+    $("#selectTitle").empty()
+    Object.keys(VideoData).map(movie => {
+        $("#titleList").append(
+            $("<div>")
+                .attr("id", movie)
+                .attr("class", "center")
+                .attr("name", movie)
+                .attr("href", "./"+movie.replace(" ", "")+".html")
+                .text(movie)
+                .click(event => this.loadEpisodes(event))
+                // .on('click', loadEpisodes(movie))
+                .append("<br/>")
+            );
+    })
+}
 
-const selectionLevel1 = document.getElementById("level2");
-
-uniqueList.forEach(item => {
-    const optiond =  document.createElement("optiond")
-    optiond.textContent = item
-    selectionLevel1.appendChild(optiond);
+$( document ).ready(function() {
+    loadMovies();
 });
 
+// const filteredArray = data.filter(r => r[0] === "movie1");
+
+// const uniqueOption = new Set();
+// filteredArray.forEach(r => uniqueOption.add(r[1]));
+
+// const uniqueList = [...uniqueOptions];
+
+// const selectionLevel1 = document.getElementById("level2");
+
+// uniqueList.forEach(item => {
+//     const optiond =  document.createElement("optiond")
+//     optiond.textContent = item
+//     selectionLevel1.appendChild(optiond);
+// });
 
 
-console.log(filteredArray);
-console.log(uniqueOption);
+
+// console.log(filteredArray);
+// console.log(uniqueOption);
